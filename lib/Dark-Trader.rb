d9e87f7e05840ec5fadaabde7f5b7b7fb2eb2414
Planes Cyril <cyrilplanes33@gmail.com>
2021-04-23 09:38:42 +0200

    Update Dark-Trader.rb

--- lib/Dark-Trader.rb
+++ lib/Dark-Trader.rb
@@ -1,28 +1,28 @@
-require 'nokogiri'
-require 'open-uri'
+require 'nokogiri' #ajout dépendance 
+require 'open-uri'#ajout dépendance 
 
 
-def create_hash()
+def create_hash() #fonction
 
-    page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))
+    page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/")) #ouverture de l'url
 
-    symbolS=[]
-    page.xpath('/html/body/div/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr/td[3]/div').each{ |a|
+    symbolS=[] # création d'un array
+    page.xpath('/html/body/div/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr/td[3]/div').each{ |a| #ciblage par xpath pui ajout du text dans l'arrey symbol
         symbolS.push(a.text)
     }
 
     priceS=[]
-    page.xpath('/html/body/div/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr/td[5]/div/a').each{ |a|
+    page.xpath('/html/body/div/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr/td[5]/div/a').each{ |a| #ciblage par xpath pui ajout du text dans l'arrey priceS
         priceS.push("$" + a.text)
     }
 
-    cryptoS = []
-    cryptoS = symbolS.zip(priceS).map! { |k, v| { k => v } }
+    cryptoS = [] #création hash
+    cryptoS = symbolS.zip(priceS).map! { |k, v| { k => v } } #utilisation du zip pour fusionner les 2 array dans le hash
     
 
 #   cryptoS.count
-    puts cryptoS
+    puts cryptoS #affichage du hash
     return cryptoS
 end
 
-create_hash
+create_hash #appelle fonction

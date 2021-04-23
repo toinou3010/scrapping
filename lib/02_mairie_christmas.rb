aee0083419683deefde1a308bcec87a016e4baba
Planes Cyril <cyrilplanes33@gmail.com>
2021-04-23 09:41:56 +0200

    Update 02_mairie_christmas.rb

--- lib/02_mairie_christmas.rb
+++ lib/02_mairie_christmas.rb
@@ -1,31 +1,31 @@
-require 'nokogiri'
-require 'open-uri'
+require 'nokogiri' #ajout dépendance 
+require 'open-uri' #ajout dépendance 
 
-mail = []
-nom = []
-url = []
-url_var = "http://annuaire-des-mairies.com/"   
-
-
+mail = [] # création d'un array
+nom = [] # création d'un array
+url = [] # création d'un array
+url_var = "http://annuaire-des-mairies.com/"   #définition de l'url de base 
 
 
 
 
-nokogiri_object = Nokogiri::HTML(open('http://annuaire-des-mairies.com/val-d-oise.html'))
-noms = nokogiri_object.xpath('//a[@class="lientxt"]/text()').each{ |c|
+
+
+nokogiri_object = Nokogiri::HTML(open('http://annuaire-des-mairies.com/val-d-oise.html')) #ouverture de l'url
+noms = nokogiri_object.xpath('//a[@class="lientxt"]/text()').each{ |c| #ciblage par xpath pui ajout du text dans l'arrey nom
 nom.push(c.text)
 }
-cities = nokogiri_object.xpath('//a[@class="lientxt"]/@href').each{ |a|
+cities = nokogiri_object.xpath('//a[@class="lientxt"]/@href').each{ |a| #ciblage par xpath pui ajout du text dans l'arrey url
  url.push(url_var + a.text)
 }
 
 
 url.each{ |b|
-open = Nokogiri::HTML(open(b))
-email = open.xpath('/html/body/div[1]/main/section[2]/div/table/tbody/tr[4]/td[2]')
+open = Nokogiri::HTML(open(b)) # ouverture de plusieur url 
+email = open.xpath('/html/body/div[1]/main/section[2]/div/table/tbody/tr[4]/td[2]') #ciblage par xpath pui ajout du text dans l'arrey mail
 mail.push(email.text)
 
 }
 
-$hash = Hash[nom.zip(mail)]
+$hash = Hash[nom.zip(mail)] #création d'un hash et fusion des 2 arrey nom et mail
 puts $hash

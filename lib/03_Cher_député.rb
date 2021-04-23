5c20531c170254380b698be5dbf0908c6c30d6a0
Planes Cyril <cyrilplanes33@gmail.com>
2021-04-23 09:45:10 +0200

    Update 03_Cher_député.rb

--- lib/03_Cher_député.rb
+++ lib/03_Cher_député.rb
@@ -1,24 +1,24 @@
-require 'nokogiri'
-require 'open-uri'
+require 'nokogiri' #ajout dépendance 
+require 'open-uri' #ajout dépendance 
 
-def get_townhall_email_city(townhall_url)
+def get_townhall_email_city(townhall_url) #fonction
 
-    mailS = []
-    nameS = []
+    mailS = [] # création d'un array
+    nameS = [] # création d'un array
 
     townhall_url.each {|url|
-    page = Nokogiri::HTML(URI.open(url))
+    page = Nokogiri::HTML(URI.open(url)) #ouverture de plusieur url
 
-    page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').each{ |mail|
+    page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').each{ |mail| #ciblage par xpath pui ajout du text dans l'arrey mailS
         mailS.push(mail.text)
     }
     
-    page.xpath('/html/body/div/main/section[1]/div/div/div/h1').each{ |a|
+    page.xpath('/html/body/div/main/section[1]/div/div/div/h1').each{ |a| #ciblage par xpath pui ajout du text dans l'arrey nameS
         name = a.text[0,a.text.size-8]
         nameS.push(name)
     }
     }
-    repertoire = []
+    repertoire = [] #création d'un hash
     repertoire = nameS.zip(mailS).map! { |k, v| { k => v } }
 
     puts repertoire

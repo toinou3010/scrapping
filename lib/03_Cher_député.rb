3803bf3ea167b9dec3b00a37959a617c3a2cc907
Toinou3010 <cyrilplanes33@gmail.com>
2021-04-23 10:19:00 +0200

    t

--- lib/03_Cher_député.rb
+++ lib/03_Cher_député.rb
@@ -1,43 +1,50 @@
-require 'nokogiri' #ajout dépendance 
-require 'open-uri' #ajout dépendance 
+require 'nokogiri'
+require 'open-uri'
 
-def get_townhall_email_city(townhall_url) #fonction
+def get_depHall_email_name(depHall_url)
 
-    mailS = [] # création d'un array
-    nameS = [] # création d'un array
+    first_nameS = []
+    last_nameS = []
+    mailS = []
 
-    townhall_url.each {|url|
-    page = Nokogiri::HTML(URI.open(url)) #ouverture de plusieur url
+    depHall_url.each {|url|
+    page = Nokogiri::HTML(URI.open(url))
 
-    page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').each{ |mail| #ciblage par xpath pui ajout du text dans l'arrey mailS
-        mailS.push(mail.text)
-    }
+        page.xpath('/html/body/div/div[2]/div/div/div/section[1]/div/article/div[3]/div/dl/dd[4]/ul/li[2]/a').each{ |mail|
+            mailS.push(mail.text)
+        }
     
-    page.xpath('/html/body/div/main/section[1]/div/div/div/h1').each{ |a| #ciblage par xpath pui ajout du text dans l'arrey nameS
-        name = a.text[0,a.text.size-8]
-        nameS.push(name)
+        page.xpath('/html/body/div/div[2]/div/div/div/section[1]/div/article/div[3]/div/dl/dd[4]/ul/li[2]/a').each{ |a|
+            first_nameS.push(a.text[0, a.text.index('.')].capitalize)
+            if a.text[a.text.index('.') + 1, a.text.index('@') - a.text.index('.') - 1] == nil
+                last_nameS.push("?")
+            else
+                last_nameS.push(a.text[a.text.index('.') + 1, a.text.index('@') - a.text.index('.') - 1].capitalize)
+            end
+        }
     }
-    }
-    repertoire = [] #création d'un hash
-    repertoire = nameS.zip(mailS).map! { |k, v| { k => v } }
 
+    repertoire = []
+    repertoire = first_nameS.zip(last_nameS, mailS)
+    repertoire.map! { |e| ["first_name", "last_name", "email"].zip(e).to_h }
+    
     puts repertoire
     return repertoire
 end
 
-def get_townhall_urls
-    page = Nokogiri::HTML(URI.open('http://annuaire-des-mairies.com/val-d-oise.html'))
+def get_depHall_urls
+    page = Nokogiri::HTML(URI.open('https://www2.assemblee-nationale.fr/deputes/liste/alphabetique'))
 
-    townhall_url=[]
-    page.xpath('//a[@class="lientxt"]/@href').each{ |a|
-        townhall_url.push("https://www.annuaire-des-mairies.com/" + a)
+    depHall_url=[]
+    page.xpath('/html/body/div/div[2]/div/div/section/div/article/div[3]/div/div[3]/div/ul/li/a/@href').each{ |a|
+        depHall_url.push("https://www2.assemblee-nationale.fr/" + a)
     }
-    return townhall_url
+    return depHall_url
 end
 
 def perform
-    townhall_url = get_townhall_urls
-    get_townhall_email_city(townhall_url)
+    depHall_url = get_depHall_urls
+    get_depHall_email_name(depHall_url)
 end
 
 perform
